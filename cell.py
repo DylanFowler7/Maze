f36754b85e38802a247dced77d62ff238f2b16b8
Skylad7 <dylan.fowler7@yahoo.com>
2024-09-13 12:51:13 -0400

    Have a grid and exit and entrance.

---
+++ cell.py
@@ -0,0 +1,62 @@
+from point import *
+
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.x1 = True
+        self.x2 = True
+        self.y1 = True
+        self.y2 = True
+        self.win = win
+
+
+    def draw(self, x1, y1, x2, y2):
+        if self.win is None:
+            return
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        if self.has_left_wall == True:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self.win.draw_line(line, "black")
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self.win.draw_line(line, "white")
+        if self.has_right_wall == True:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self.win.draw_line(line, "black")
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self.win.draw_line(line, "white")
+        if self.has_top_wall == True:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self.win.draw_line(line, "black")
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self.win.draw_line(line, "white")
+        if self.has_bottom_wall == True:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self.win.draw_line(line, "black")
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self.win.draw_line(line, "white")
+
+    def draw_move(self, to_cell, undo=False):
+        half_length = abs(self.x2 - self.x1) // 2
+        x_center = half_length + self.x1
+        y_center = half_length + self.y1
+
+        half_length2 = abs(to_cell.x2 - to_cell.x1) // 2
+        x_center2 = half_length2 + to_cell.x1
+        y_center2 = half_length2 + to_cell.y1
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self.win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from window import *
-from point import Point, Line
+from maze import *
 
 def main():
-    win = Window(800, 600)
-    p1 = Point(200, 300)
-    p2 = Point(300, 500)
-    line1 = Line(p1,p2)
-    win.draw_line(line1, "black")
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+
+
+
     win.wait_for_close()
 
-if __name__ == '__main__':
-    main()
 
+main()
